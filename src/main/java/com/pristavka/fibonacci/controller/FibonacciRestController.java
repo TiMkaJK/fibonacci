8162a8a20a21a5f766035738c63b37fbb924acc0
dpristavka <timka.jk@gmail.com>
2022-05-19 18:38:19 +0300

    add fibonacci values

--- src/main/java/com/pristavka/fibonacci/controller/FibonacciRestController.java
+++ src/main/java/com/pristavka/fibonacci/controller/FibonacciRestController.java
@@ -3,11 +3,11 @@ package com.pristavka.fibonacci.controller;
 import com.pristavka.fibonacci.service.FibonacciService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,6 +19,12 @@ public class FibonacciRestController {
     @GetMapping(path = "/{value}")
     public ResponseEntity<Integer> getFibonacci(@PathVariable(name = "value") @NonNull Integer value) {
         return ResponseEntity.ok(this.fibonacciService.getFibonacci(value));
+    }
+
+    @ExceptionHandler(value = {NoSuchElementException.class})
+    public ResponseEntity exception(NoSuchElementException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
     }
 }
 
